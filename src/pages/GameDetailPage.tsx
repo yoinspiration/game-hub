f79c948e4e0aa59c14d17a52fa6657d673d77c19
Frank <yoinspiration@outlook.com>
2024-06-21 14:46:57 +0800

    Build game screenshots

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -4,6 +4,7 @@ import ExpandableText from "../components/ExpandableText";
 import GameAttributes from "../components/GameAttributes";
 import useGame from "../hooks/useGame";
 import GameTrailer from "../components/GameTrailer";
+import GameScreenshots from "../components/GameScreenshots";
 
 const GameDetailPage = () => {
   const { slug } = useParams();
@@ -22,6 +23,8 @@ const GameDetailPage = () => {
       <GameAttributes game={game} />
 
       <GameTrailer gameId={game.id} />
+
+      <GameScreenshots gameId={game.id} />
     </>
   );
 };
